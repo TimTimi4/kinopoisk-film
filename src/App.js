85d5a1231357a901f0dc5efba24a5e765e9e3ea7
TimTimi4 <doronin200894@gmail.com>
2021-09-04 12:27:43 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import FilmText from "./FilmInfo/FilmText";
+
+const data = {
+	name: 'Черный ястреб (2001)',
+	img: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1600647/59a72eaa-3bf3-448b-8da4-5d5ed7375df8/300x450',
+	actors: [{id:1, name:'Джош Хартнетт'},{id:2, name:'Юэн Макгрегор'},{id:3, name:'Эрик Бана'}],
+	rating: '7.816',
+	url: 'https://www.kinopoisk.ru/film/612/'
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className='wrapper'>
+		<h1 className="filmTitle">{data.name}</h1>
+		<img src={data.img} alt="#" />
+
+		<FilmText data={data.actors}/>
+		<div className="rating">Рейтинг IMDB: <strong>{data.rating}</strong></div>
+		<div className="linkFilm">{data.url}</div>
+
+	</div>
   );
 }
 
---
+++ src/FilmInfo/FilmActors.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function FilmActors({actor}){
+	return <li>{actor.name}</li>
+}
